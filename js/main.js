b7f89b04780b9057979391d167ea388b29591c37
renjitha123 <ranjithabnair@gmail.com>
2022-02-12 16:45:55 +0530

    third commit
    this is my third commit

--- js/main.js
+++ js/main.js
@@ -107,30 +107,32 @@ var owl6 = $('.owl-blog-item');
     /*------------------------
 		Testimonial Slider
     ----------------------- */
- var x, i, j, l, ll, selElmnt, a, b, c;
+	/*
+	
+ var x, i, j, l, ll, selElmnt, a, b, c;*/
 /*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName("custom-select");
+/*x = document.getElementsByClassName("custom-select");
 l = x.length;
 for (i = 0; i < l; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
+  ll = selElmnt.length;*/
   /*for each element, create a new DIV that will act as the selected item:*/
-  a = document.createElement("DIV");
+ /* a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
+  x[i].appendChild(a);*/
   /*for each element, create a new DIV that will contain the option list:*/
-  b = document.createElement("DIV");
+ /* b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
+  for (j = 1; j < ll; j++) {*/
     /*for each option in the original select element,
     create a new DIV that will act as an option item:*/
-    c = document.createElement("DIV");
+   /* c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
+    c.addEventListener("click", function(e) {*/
         /*when an item is clicked, update the original select box,
         and the selected item:*/
-        var y, i, k, s, h, sl, yl;
+        /*var y, i, k, s, h, sl, yl;
         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
         sl = s.length;
         h = this.parentNode.previousSibling;
@@ -152,19 +154,19 @@ for (i = 0; i < l; i++) {
     b.appendChild(c);
   }
   x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
+  a.addEventListener("click", function(e) {*/
       /*when the select box is clicked, close any other select boxes,
       and open/close the current select box:*/
-      e.stopPropagation();
+    /*  e.stopPropagation();
       closeAllSelect(this);
       this.nextSibling.classList.toggle("select-hide");
       this.classList.toggle("select-arrow-active");
     });
 }
-function closeAllSelect(elmnt) {
+function closeAllSelect(elmnt) {*/
   /*a function that will close all select boxes in the document,
   except the current select box:*/
-  var x, y, i, xl, yl, arrNo = [];
+ /* var x, y, i, xl, yl, arrNo = [];
   x = document.getElementsByClassName("select-items");
   y = document.getElementsByClassName("select-selected");
   xl = x.length;
@@ -181,10 +183,10 @@ function closeAllSelect(elmnt) {
       x[i].classList.add("select-hide");
     }
   }
-}
+}*/
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);   
+/*document.addEventListener("click", closeAllSelect);   */
     /*------------------
         FullHeight  banner
     --------------------*/
